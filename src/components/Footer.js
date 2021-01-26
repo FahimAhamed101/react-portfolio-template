13390b2348029412fbf6146202ebe04d5717a6de
Fahim ahamed <54832397+FahimAhamed101@users.noreply.github.com>
2021-01-26 06:59:23 -0800

    Update Footer.js
    good

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,7 +27,7 @@ const Footer = () => {
           </div>
           <span>
             Made With <icon>❤</icon> by{" "}
-            <a href="https://www.chetanverma.com/">Chetan Verma</a>
+            <a href="https://fahimahamed101.netlify.app/">Fahim Ahamed</a>
           </span>
         </div>
       </div>
